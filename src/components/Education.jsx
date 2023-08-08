e3416644ef0d0851b2ab71db49d14c13cc9be99d
Dhruv Shelke <dhruvshelke588@gmail.com>
2023-08-08 13:13:05 +0530

    Few changes in UI and bug fixes

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,52 +1,52 @@
-import React from "react";
+import React from 'react';
 import {
   VerticalTimeline,
   VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import { motion } from "framer-motion";
+} from 'react-vertical-timeline-component';
+import { motion } from 'framer-motion';
 
-import "react-vertical-timeline-component/style.min.css";
+import 'react-vertical-timeline-component/style.min.css';
 
-import { styles } from "../styles";
-import { education } from "../constants";
-import { SectionWrapper } from "../hoc";
-import { textVariant } from "../utils/motion";
+import { styles } from '../styles';
+import { education } from '../constants';
+import { SectionWrapper } from '../hoc';
+import { textVariant } from '../utils/motion';
 
 const EducationCard = ({ education }) => {
   return (
     <VerticalTimelineElement
       contentStyle={{
-        background: "#1d0D36",
-        color: "#fff",
+        background: '#1d0D36',
+        color: '#fff',
       }}
-      contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+      contentArrowStyle={{ borderRight: '7px solid  #232631' }}
       date={education.date}
       iconStyle={{ background: education.iconBg }}
       icon={
-        <div className='flex justify-center items-center w-full h-full'>
+        <div className="flex justify-center items-center w-full h-full">
           <img
             src={education.icon}
             alt={education.company_name}
-            className='w-[60%] h-[60%] object-contain'
+            className="w-[60%] h-[60%] object-contain"
           />
         </div>
       }
     >
       <div>
-        <h3 className='text-white text-[24px] font-bold'>{education.title}</h3>
+        <h3 className="text-white text-[24px] font-bold">{education.title}</h3>
         <p
-          className='text-secondary text-[16px] font-semibold'
+          className="text-secondary text-[16px] font-semibold"
           style={{ margin: 0 }}
         >
           {education.company_name}
         </p>
       </div>
 
-      <ul className='mt-5 list-disc ml-5 space-y-2'>
+      <ul className="mt-5 list-disc ml-5 space-y-2">
         {education.points.map((point, index) => (
           <li
             key={`education-point-${index}`}
-            className='text-white-100 text-[14px] pl-1 tracking-wider'
+            className="text-white-100 text-[14px] pl-1 tracking-wider"
           >
             {point}
           </li>
@@ -60,21 +60,16 @@ const Education = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-      <p className={`${styles.sectionSubText} text-center`}>
+        <p className={`${styles.sectionSubText} text-center`}>
           What I have learnt so far..
         </p>
-        <h2 className={`${styles.sectionHeadText} text-center`}>
-          Education
-        </h2>
+        <h2 className={`${styles.sectionHeadText} text-center`}>Education</h2>
       </motion.div>
 
-      <div className='mt-20 flex flex-col'>
+      <div className="mt-20 flex flex-col">
         <VerticalTimeline>
           {education.map((education, index) => (
-            <EducationCard
-              key={`education-${index}`}
-              education={education}
-            />
+            <EducationCard key={`education-${index}`} education={education} />
           ))}
         </VerticalTimeline>
       </div>
@@ -82,4 +77,4 @@ const Education = () => {
   );
 };
 
-export default SectionWrapper(Education, "education");
+export default SectionWrapper(Education, 'education');
