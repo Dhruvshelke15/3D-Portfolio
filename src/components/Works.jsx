117c91be613c5d5fdefbd54bac5a3b99b31213ff
Dhruv Shelke <dhruvshelke588@gmail.com>
2023-12-28 17:14:50 +0530

    removed project section animations

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -83,14 +83,13 @@ const ProjectCard = ({
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
       <div className="w-full flex">
         <motion.p
-          variants={fadeIn('', '', 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           The projects below are instances of my work in the real world that
